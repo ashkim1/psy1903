f2721b24e0a6b5312e9ec0a26c3802586b73048f
Ash Kim <ashkim@college.harvard.edu>
2024-09-10 17:36:21 -0400

    Task Set 2, Q6: math-response-time.js

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,18 @@
+// Welcome alert
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.")
+
+// Equations
+let trial1 = prompt("What is 4 + 3?");
+console.log(trial1);
+alert("You answered " + trial1)
+
+let trial2 = prompt("What is 2 + 6?");
+console.log(trial2);
+alert("You answered " + trial2)
+
+let trial3 = prompt("What is 1 + 5?");
+console.log(trial3);
+alert("You answered " + trial3)
+
+// Thank you alert
+alert("Thank you for your participation!")
