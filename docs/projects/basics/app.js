f852761276ab9f3a81bab8b45d3a29571fe316eb
Ash Kim <ashkim@college.harvard.edu>
2024-09-15 16:35:51 -0400

    Week 2 in class and video notes

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -30,16 +30,16 @@ let correct = true;
 console.log(typeof correct); //boolean
 console.log(correct); // true
 
-console.log(10 > 15); // false
+/* console.log(10 > 15); // false
 console.log(10 <= 15); // true
 
 let age = 25;
 let adult = age > 18;
 
 console.log(adult); // true 
-console.log(typeof adult); // boolean
+console.log(typeof adult); // boolean */
 
-// An Array of Numbers
+/* // An Array of Numbers
 let scores = [75.5, 91, 91.6, 92.3, 45.5];
 
 // An Array of Strings
@@ -52,4 +52,34 @@ let experiment = {
     name: 'Stroop',
     colors: ['red', 'green', 'blue'],
     trialCount: 10
-};
+}; */
+
+// let response1 = prompt("What is 5 + 3?");
+// console.log(response1); 
+
+// let response2 = prompt("What is prompt 5 + 5?");
+/* Do not include 'let' before variable name because you are reassigning, not redefining.
+In this case, this showed 2 prompts in the browser. But you can number them too. */
+// console.log(response2);
+
+// Question 4:
+/* console.log(Hello World);
+console.log("Hello World');   
+console.log('Hello World'); */
+console.log("Hello World");
+
+// Question 5:
+let courseName = "Programming for Psychologists";
+console.log(courseName.length); // Output: 29
+
+// Output the value for courseName translated to all capital letters.
+// Expected result: PROGRAMMING FOR PSYCHOLOGISTS
+console.log(courseName.toUpperCase());
+
+// Output the index position of the word 'Psychologists' within the string stored in courseName.
+// Expected result: 16
+console.log(courseName.indexOf("Psychologists"));
+
+// Output the value for courseName with the word 'Programming' replaced with the word 'Coding'.
+// Expected result: "Coding for Psychologists"
+console.log([updated = courseName.replace("Programming", "Coding")]);
