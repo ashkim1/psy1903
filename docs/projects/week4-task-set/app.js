000927161d3dc6665280f987ae1628b24a7391e1
Ash Kim <ashkim@college.harvard.edu>
2024-09-24 17:05:52 -0400

    celciusToFahrenheit

---
+++ docs/projects/week4-task-set/app.js
@@ -0,0 +1,8 @@
+// celciusToFahrenheit
+function celciusToFahrenheit(tempC) {
+    let tempF = (tempC * 1.8) + 32;
+    return tempF;
+}
+
+console.log(celciusToFahrenheit(10)); // 50
+console.log(celciusToFahrenheit(-5)); // 23
