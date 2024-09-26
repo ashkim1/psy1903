d2c6b76032584128c931414d0c97849f3f416f78
Ash Kim <ashkim@college.harvard.edu>
2024-09-26 15:32:29 -0400

    convertTemp

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,8 +1,23 @@
-// celciusToFahrenheit
-function celciusToFahrenheit(tempC) {
-    let tempF = (tempC * 1.8) + 32;
-    return tempF;
+// // celciusToFahrenheit
+// function celciusToFahrenheit(tempC) {
+//     let tempF = (tempC * 1.8) + 32;
+//     return tempF;
+// }
+
+// console.log(celciusToFahrenheit(10)); // 50
+// console.log(celciusToFahrenheit(-5)); // 23
+
+
+
+// convertTemp
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        return ((temp - 32) / 1.8);
+    } else if (convertTo == 'f') {
+        return ((temp * 1.8) + 32);
+    }
 }
 
-console.log(celciusToFahrenheit(10)); // 50
-console.log(celciusToFahrenheit(-5)); // 23
+console.log(convertTemp(10, 'c')); // -12.2222
+console.log(convertTemp(10, 'f')); // 50
