98fb4fa93112104d81d32d3e5cc98b95bca290ec
Ash Kim <ashkim@college.harvard.edu>
2024-09-28 17:01:40 -0400

    filterNumbers

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -56,17 +56,45 @@
 
 
 
-// getOddNumbers
+// // getOddNumbers
 
-function getOddNumbers(numbers) {
+// function getOddNumbers(numbers) {
+//     let results = [];
+//     for (let number of numbers) {
+//         if (number % 2 == 1) {
+//             results.push(number);
+//         }
+//     }
+//     return results;
+// }
+
+// console.log(getOddNumbers([1, 2, 3, 4, 5])); // [1, 3, 5]
+// console.log(getOddNumbers([12, 45, 10, 11, 61])); // [45, 11, 61]
+
+
+
+// filterNumbers
+
+function filterNumbers(numbers, evenOrOdd) {
     let results = [];
-    for (let number of numbers) {
-        if (number % 2 == 1) {
-            results.push(number);
+    if (evenOrOdd == 'even') {
+        for (let number of numbers) {
+            if (number % 2 == 0) {
+                results.push(number);
+            }
+        }
+    } else if (evenOrOdd == 'odd') {
+        for (let number of numbers) {
+            if (number % 2 == 1) {
+                results.push(number);
+            }
         }
     }
     return results;
 }
 
-console.log(getOddNumbers([1, 2, 3, 4, 5])); // [1, 3, 5]
-console.log(getOddNumbers([12, 45, 10, 11, 61])); // [45, 11, 61]
+console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // [2, 4]
+console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // [1, 3, 5]
+
+console.log(filterNumbers([45, 10, 11, 61], 'even')); // [10]
+console.log(filterNumbers([45, 10, 11, 61], 'odd')); // [45, 11, 61]
