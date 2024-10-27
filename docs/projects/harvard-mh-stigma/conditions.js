b31c0efcb9046ffd76412e58b151236bd230853c
Ash Kim <ashkim@college.harvard.edu>
2024-10-27 18:08:11 -0400

    Updates to IAT code

--- docs/projects/harvard-mh-stigma/conditions.js
+++ docs/projects/harvard-mh-stigma/conditions.js
@@ -1,42 +1,27 @@
-// // Modify the following `blocksA`, `blocksB`, and `words` arrays with appropriate values for your experiment
-// let blocksA = [
-//     ['mental health conditions', 'physical health conditions'],
-//     ['humanizing words', 'stigmatizing words']
-// ];
-
-// let blocksB = [
-//     ['mental health conditions or humanizing words', 'physical health conditions or stigmatizing words'],
-//     ['mental health conditions or stigmatizing words', 'physical health conditions or humanizing words']
-// ];
-
+// Modify the following `blocksA`, `blocksB`, and `words` arrays with appropriate values for your experiment
 let blocksA = [
-    ['men', 'women'],
-    ['family', 'career']
+    ['mental health conditions', 'mental health treatment'],
+    ['humanizing words', 'stigmatizing words']
 ];
 
 let blocksB = [
-    ['family or men', 'career or women'],
-    ['family or women', 'career or men']
+    ['mental health conditions or humanizing words', 'mental health treatment or stigmatizing words'],
+    ['mental health conditions or stigmatizing words', 'mental health treatment or humanizing words']
 ];
-
 
 // There should be 10 words per category
 let words = {
-    men: ['Calvin', 'Ben', 'Jamal', 'Charles', 'Mateo', 'Henry', 'Lucas', 'Andrew', 'Darnell', 'George'],
-    women: ['Mariah', 'Emily', 'Julia', 'Brenda', 'Tasha', 'Priya', 'Kelsey', 'Jasmine', 'Grace', 'Camila'],
-    family: ['home', 'wedding', 'children', 'relative', 'parent', 'cousin', 'sibling', 'spouse', 'house', 'baby'],
-    career: ['corporation', 'salary', 'interview', 'promotion', 'resume', 'office', 'income', 'hiring', 'bosses', 'corporate'],
+    'mental health conditions': ['depression', 'anxiety', 'OCD', 'PTSD', 'eating disorder', 'body dysmorphia', 'bipolar disorder'],
+    'mental health treatment': ['therapy', 'medication', 'support group', 'counseling', 'psychiatry', 'CAMHS', 'peer counseling', 'SHARE counselors', 'tutors', 'resident deans'],
+    'humanizing words': ['capable', 'normal', 'challenging', 'feeling', 'sane', 'rational', 'realistic', 'human'],
+    'stigmatizing words': ['crazy', 'insane', 'psychotic', 'unstable', 'weak', 'fragile', 'dangerous', 'unpredictable', 'weird', 'useless'],
 };
-
-
 
 // Your final experiment should show 36 words per category; 
 // when developing your experiment you can reduce this number
 // to expedite the process of testing the experiment
 // Always set the count to an even number
 let count = 4;
-
-
 
 // The following code will process the above information into a `conditions` array you 
 // can use to structure your experiment. 
@@ -48,18 +33,26 @@ console.log(conditions);
 
 function generateConditions() {
 
+    // Define an empty array we'll accumulate our conditions in
     let conditions = [];
 
+    // Define a blocks array that will start with blocksA in random order, followed by blocksB in random order
     let blocks = shuffle(blocksA).concat(shuffle(blocksB));
 
+    // Loop through the categories of our blocks
     for (let categories of blocks) {
 
+        // Define an empty array we'll accumulate our trials in
         let trials = [];
 
+        // Loop through the individual categories
         for (let category of shuffle(categories)) {
 
+            // wordChoices is an array we’ll build pulling from our pool of words
             let wordChoices = [];
 
+            // In the case of compound categories (e.g. "women or career") we need to split
+            // this into the individual categories (e.g. "women" and "career") to build our pool of words
             if (category.includes('or')) {
                 let parts = category.split(' or ');
                 wordChoices = shuffle(words[parts[0]].concat(words[parts[1]]));
@@ -67,23 +60,34 @@ function generateConditions() {
                 wordChoices = shuffle(words[category]);
             }
 
+            // Start with a copy of wordChoices, which we'll pull from until it's empty, at which point we'll start with a new copy
+            // FYI the ... operator is used here to make a copy of the wordChoices array
             let wordsLeft = [...wordChoices];
 
+            // Now we'll pick our words on a loop. We loop for count/2
+            // because this will happen twice (once for each category in the block)
             for (let i = 0; i < count / 2; i++) {
 
+                // If we've emptied our wordChoices array, "refill" it
                 if (wordsLeft.length == 0) {
                     wordsLeft = shuffle([...wordChoices]);
                 }
 
+                // Pick a random word
                 let word = sample(wordsLeft);
+
+                // Remove the word we just picked so we don't pick it again
                 remove(wordsLeft, (element) => element === word);
 
+                // If the category matches the left category, we expect "f" key response
                 if (categories[0] == category) {
                     expectedResponse = 'f';
+                    // Otherwise we expect a "j" key response
                 } else {
                     expectedResponse = 'j';
                 }
 
+                // Push an object of trial data onto our trials array
                 trials.push({
                     word: word,
                     expectedCategory: searchObject(words, word),
@@ -93,6 +97,7 @@ function generateConditions() {
             }
         }
 
+        // Push an object of block data onto our conditions array
         conditions.push({
             categories: categories,
             trials: shuffle(trials)
@@ -106,6 +111,7 @@ function generateConditions() {
 /**
 * The following are miscellaneous array functions utilized in the above code
 */
+
 // Shuffle an array
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
--- docs/projects/harvard-mh-stigma/experiment.js
+++ docs/projects/harvard-mh-stigma/experiment.js
@@ -21,22 +21,86 @@ let welcomeTrial = {
 timeline.push(welcomeTrial);
 
 
+// Video Trial //
+let videoTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: ` 
+    <h1> <span class = 'title'>Task 1 of 3</span></h1>
+    <p> Please watch the following video </p>
+    <iframe width="560" height="315" 
+        src="https://www.youtube.com/embed/AYAHkql75qM?si=OVFCmPnPwVTmPB3K" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media;gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+        tabindex="-1">
+        </iframe>
+    <p> Press the <span class = 'key'>SPACE</span> key when you have completed the video and are ready to move on to the next task. </p>
+    `,
+    choices: [' '],
+
+};
+timeline.push(videoTrial);
+
+// Survey Trial //
+// Define likert scale
+let likertScale = [
+    "Strongly Disagree",
+    "Disagree",
+    "Neutral",
+    "Agree",
+    "Strongly Agree"
+];
+
+// Survey questions
+let survey = {
+    type: jsPsychSurveyLikert,
+    questions: [
+        { prompt: "I feel comfortable expressing my feelings.", labels: likertScale },
+        { prompt: "I feel that my mental health is valued at Harvard.", labels: likertScale },
+        { prompt: "At Harvard, I feel like I belong.", labels: likertScale },
+        { prompt: "Mental health is something that should be taken seriously.", labels: likertScale },
+        { prompt: "It is normal to have issues with mental health.", labels: likertScale },
+        { prompt: "Physical health issues", labels: likertScale },
+    ],
+    randomize_question_order: true
+};
+timeline.push(survey);
+
+
+// IAT //
+//Beginning the outer loop
+//Establishing counter for the four-part block seperation screens
+let counter = 1;
+
 for (let block of conditions) {
 
-    // Screen with instructions, indicating the two categories
-    // and the expected keys to be pressed
+    // Setting left and right category variables
     let leftCategory = block.categories[0];
     let rightCategory = block.categories[1];
 
-    for (let trial of block.trials) {
-        // Screen that displays trial.word in the center
-        // as well as the left/right categories
-        // listening for key response (f,j)
-        // on_finish: process the response, store the appropriate data
+    // Screen with instructions, indicating the two categories
+    let blockintroTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `
+        <h1><span class = 'title'>Part ${counter++} of 4</span></h1> 
+        <p>In this part, the two categories will be: <span class = 'bold'>${leftCategory}</span> and <span class = 'bold'>${rightCategory}</span></p>
+        <p>If the word you see in the middle of the screen should be sorted into the <span class = 'bold'>${leftCategory}</span> category, press the <span class = 'key'>F</span> key.</p>
+        <p>If thee word you see in the middle of the screen should be sorted into the  <span class = 'bold'>${rightCategory}</span> category, press the <span class = 'key'>J</span> key.</p>
+        <p>Press the <span class = 'key'>SPACE</span> to begin.</p>  
+        `,
+        choices: [' '],
+    }
+    timeline.push(blockintroTrial);
 
-        let example = {
+    //Trial inner loop
+    for (let trial of block.trials) {
+        let iatTrial = {
             type: jsPsychHtmlKeyboardResponse,
-            stimulus: `...`,
+            stimulus: `
+            <span class= 'category1' > <strong>${leftCategory}</strong> (press F)</span>
+            <span class='category2'> <strong>${rightCategory}</strong> (press J)</span>
+            <p class='word'>${trial.word}</p>`,
+            choices: ['f', 'j'],
             data: {
                 collect: true,
                 trialType: 'iat',
@@ -44,17 +108,98 @@ for (let block of conditions) {
                 expectedCategory: trial.expectedCategory,
                 expectedCategoryAsDisplayed: trial.expectedCategoryAsDisplayed,
                 leftCategory: leftCategory,
-                rightCategory: rightCategory
+                rightCategory: rightCategory,
             },
             on_finish: function (data) {
-                // if data.response == trial.expectedResponse
-                // data.correct = true
-                // else
-                // data.correct = false
+                if (data.response == trial.expectedResponse) {
+                    iatTrial.data.correct = true;
+                } else {
+                    iatTrial.data.correct = false;
+                }//not finished yet 
             }
+
         }
-        timeline.push(example);
+        timeline.push(iatTrial);
+
+        //Adding the fixation trial inbetween iatTrials
+        let fixationTrial = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `+ `,
+            trial_duration: 250,
+            choices: ['NO KEY']
+        }
+        timeline.push(fixationTrial);
     }
 }
+
+// Results Trial //
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: `
+        <h1>Please wait...</h1>
+        <span class='loader'></span>
+        <p>We are saving the results of your inputs.</p>
+        `,
+    on_start: function () {
+        let prefix = 'iat';
+        let dataPipeExperimentId = 'Ib1PGFLsDH5z';
+        let forceOSFSave = false;
+
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+            .csv();
+
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            jsPsych.finishTrial();
+        })
+    }
+}
+timeline.push(resultsTrial);
+
+
+// Debrief //
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you for your participation!</h1> 
+    <p>The experiment is complete. You can now close this tab.</p>
+    `,
+    choices: ['NO KEYS'],
+    on_start: function () {
+        let data = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
+            .csv();
+        console.log(data);
+    }
+};
+timeline.push(debriefTrial);
+
 
 jsPsych.run(timeline);
