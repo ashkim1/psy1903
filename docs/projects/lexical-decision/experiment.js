df7858d94e4d3ee09c02a57cddaadd110aaab145
Ash Kim <ashkim@college.harvard.edu>
2024-10-28 15:28:46 -0400

    week 9 updates

--- docs/projects/lexical-decision/experiment.js
+++ docs/projects/lexical-decision/experiment.js
@@ -1,8 +1,34 @@
 // Initialize the jsPsych library
-let jsPsych = initJsPsych();
+let jsPsych = initJsPsych({
+    show_progress_bar: true
+});
 
 // Define the timeline as an empty array where we will add all our trials
 let timeline = [];
+
+// Age Check: Abort Experiment //
+let ageCheckTrial = {
+    type: jsPsychSurveyHtmlForm,
+    html: `
+    <h1>Welcome!</h1> 
+    Please enter your age to continue: <input type='text' name='age' id='age'>
+    `,
+    autofocus: 'age',
+    on_finish: function (data) {
+        if (data.response.age < 18) {
+            jsPsych.abortExperiment('You must be 18 years or older to complete this experiment.');
+        }
+    }
+}
+timeline.push(ageCheckTrial);
+
+// // Full Screen //
+// let enterFullScreenTrial = {
+//     type: jsPsychFullscreen,
+//     fullscreen_mode: true
+// };
+
+timeline.push(enterFullScreenTrial);
 
 // CSS //
 let colors = jsPsych.randomization.repeat(['red', 'green', 'blue'], 1);
@@ -34,6 +60,29 @@ timeline.push(welcomeTrial); // add to welcome trial to timeline array
 
 
 // CONDITIONS CODE IN DIFF FILE FOR ORGANIZATION PURPOSES //
+
+// Prime Trial //
+let primeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+        <p>You were randomly chosen to see this trial.</p> 
+        <p>Press the <span class='key'>SPACE</span> key to continue.</p>
+        `,
+    choices: [' '],
+    data: {
+        collect: true,
+        trialType: 'prime',
+    },
+    on_load: function () {
+        if (getRandomNumber(0, 1) == 0) {
+            jsPsych.data.addProperties({ sawPrime: false });
+            jsPsych.finishTrial();
+        } else {
+            jsPsych.data.addProperties({ sawPrime: true });
+        }
+    }
+}
+timeline.push(primeTrial);
 
 
 // Shuffle the conditions
@@ -76,6 +125,21 @@ for (let block of conditions) {
             }
         }
         timeline.push(conditionTrial);
+
+        let feedbackTrial = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `<h1>Incorrect</h1>`,
+            trial_duration: 1000,
+            choices: ['NO KEY'],
+            on_load: function () {
+                let lastTrialData = jsPsych.data.getLastTrialData().values()[0];
+                if (lastTrialData.correct) {
+                    // Force skip this feedback trial if they got the previous trial correct
+                    jsPsych.finishTrial();
+                }
+            },
+        }
+        timeline.push(feedbackTrial);
     }
 }
 
@@ -114,7 +178,12 @@ let resultsTrial = {
 }
 timeline.push(resultsTrial);
 
-
+// // Exit Full Screen //
+// let exitFullScreenTrial = {
+//     type: jsPsychFullscreen,
+//     fullscreen_mode: false
+// };
+// timeline.push(exitFullScreenTrial);
 
 // Debrief
 let debriefTrial = {
@@ -125,6 +194,7 @@ let debriefTrial = {
     `,
     choices: ['NO KEYS'],
     on_start: function () {
+        jsPsych.progressBar.progress = 1;
         let data = jsPsych.data
             .get()
             .filter({ collect: true })
